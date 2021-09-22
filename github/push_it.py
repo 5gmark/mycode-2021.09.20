7016168d8ff62207e19d56663bc9acc19bb1f2de
5gmark <5gmark@users.noreply.github.com>
2021-09-22 14:50:21 +0000

    aoeu

--- github/push_it.py
+++ github/push_it.py
@@ -15,29 +15,34 @@ import getopt
 import os
 import sys
 
-argumentList = sys.argv[1:]
-options      = "v"
-long_options = ["version"]
-version      = '1.3'
 
-try:
-  arguments, values = getopt.getopt(argumentList, options, long_options)
-  for currentArgument, currentValue in arguments:
-    if currentArgument in ("-v", "--version"):
-      print ("pushit " + version)
-except:
-  pass
 
-commit_message    = input('Commit Comment: ')
-#working_directory = 'cd ~/mycode'
-working_directory = '/home/student/mycode'
-git_add           = 'git add *'
-git_commit        = 'git commit -m "' + commit_message + '"'
-git_push          = 'git push origin'
+def main():
+  argumentList = sys.argv[1:]
+  options      = "v"
+  long_options = ["version"]
+  version      = '1.3'
+  try:
+    arguments, values = getopt.getopt(argumentList, options, long_options)
+    for currentArgument, currentValue in arguments:
+      if currentArgument in ("-v", "--version"):
+        print ("pushit " + version)
+  except:
+    pass
 
-#os.system(working_directory)
-os.chdir(working_directory)
-os.system(git_add)
-os.system(git_commit)
-os.system(git_push)
+  commit_message    = input('Commit Comment: ')
+  #working_directory = 'cd ~/mycode'
+  working_directory = '/home/student/mycode'
+  git_add           = 'git add *'
+  git_commit        = 'git commit -m "' + commit_message + '"'
+  git_push          = 'git push origin'
+
+  #os.system(working_directory)
+  os.chdir(working_directory)
+  os.system(git_add)
+  os.system(git_commit)
+  os.system(git_push)
+
+if __name__ == "__main__":
+    main()
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
